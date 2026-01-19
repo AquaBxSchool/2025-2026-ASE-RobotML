a584b1e1832da7a65bcec617e4cf726f1dd5d61d
Tom <35542432+AquaBx@users.noreply.github.com>
2026-01-19 15:40:35 +0100

    ast visitor

---
+++ RobotML/packages/cli/src/ast.ts
@@ -0,0 +1,302 @@
+import { createRobotMlServices } from "robot-ml-language";
+import type {
+    VariableDeclaration as VariableDeclarationT,
+    FunctionDeclaration as FunctionDeclarationT,
+    RobotML as RobotMLT,
+    Block as BlockT,
+    Statement as StatementT,
+    Assignation as AssignationT,
+    Backward as BackwardT,
+    Condition as ConditionT,
+    Forward as ForwardT,
+    FunctionCall as FunctionCallT,
+    Leftward as LeftwardT,
+    Mouvement as MouvementT,
+    Rightward as RightwardT,
+    Rotate as RotateT,
+    SetClock as SetClockT,
+    setSpeed as SetSpeedT,
+    Expression as ExpressionT,
+
+    Binary as BinaryT,
+    Operation as OperationT,
+    GetClock as GetClockT,
+    GetSensor as GetSensorT,
+    GetSpeed as GetSpeedT,
+    Unary as UnaryT,
+    VariableRef as VariableRefT,
+    BinaryOp,
+
+} from "robot-ml-language";
+import { EmptyFileSystem, Reference } from "langium";
+import { parseHelper } from "langium/test";
+
+interface Visitor { }
+interface Statement { }
+interface Expression { }
+
+class Robot implements Visitor {
+    functions: FunctionDeclaration[]
+
+    constructor(robot: RobotMLT) {
+        this.functions = robot.functions.map(el => new FunctionDeclaration(el))
+    }
+}
+
+class FunctionDeclaration implements Visitor, Statement {
+    name: string
+    block
+    returnType: string
+    parameters: VariableDeclaration[]
+
+    constructor(el: FunctionDeclarationT) {
+        this.name = el.name
+        this.block = new Block(el.block)
+        this.returnType = el.returnType.$type
+        this.parameters = el.parameters.map(el => new VariableDeclaration(el))
+    }
+}
+
+class VariableDeclaration implements Visitor, Statement {
+
+    name: string
+    type?: string
+
+    constructor(el: VariableDeclarationT) {
+        this.name = el.name
+        this.type = el.type?.$type
+    }
+}
+
+class Block implements Visitor, Statement {
+    statements: Statement[]
+
+    constructor(el: BlockT) {
+        this.statements = el.statements.map(StatementVisit)
+    }
+}
+
+class Assignation implements Visitor, Statement {
+    decl: VariableDeclaration
+    expression : Expression
+    constructor(el: AssignationT) {
+        this.expression = ExpressionVisit(el.expression)
+        this.decl = new VariableDeclaration(el.variableDecl)
+    }
+}
+class Backward implements Visitor, Statement {
+    constructor(el: BackwardT) {
+        throw `Not Implemented ${el.$type}`
+    }
+}
+class Condition implements Visitor, Statement {
+    blocks: Block[]
+    expression: Expression[]
+
+    constructor(el: ConditionT) {
+        this.expression = el.conditions.map(ExpressionVisit)
+        this.blocks = el.block.map(el => new Block(el))
+        this.blocks = el.block.map(el => new Block(el))
+    }
+}
+class Forward implements Visitor, Statement {
+    constructor(el: ForwardT) {
+        throw `Not Implemented ${el.$type}`
+    }
+}
+class FunctionCall implements Visitor, Statement, Expression {
+    parameters : string[]
+    decl : string
+    constructor(el: FunctionCallT) {
+        this.decl = ReferenceParse(el.functiondeclaration)
+        this.parameters = el.parameters.map(ReferenceParse)
+    }
+}
+class Leftward implements Visitor, Statement {
+    constructor(el: LeftwardT) {
+        throw `Not Implemented ${el.$type}`
+    }
+}
+class Mouvement implements Visitor, Statement {
+    constructor(el: MouvementT) {
+        throw `Not Implemented ${el.$type}`
+    }
+}
+class Rightward implements Visitor, Statement {
+    constructor(el: RightwardT) {
+        throw `Not Implemented ${el.$type}`
+    }
+}
+class Rotate implements Visitor, Statement {
+    constructor(el: RotateT) {
+        throw `Not Implemented ${el.$type}`
+    }
+}
+class SetClock implements Visitor, Statement {
+    constructor(el: SetClockT) {
+        throw `Not Implemented ${el.$type}`
+    }
+}
+class SetSpeed implements Visitor, Statement {
+    constructor(el: SetSpeedT) {
+        throw `Not Implemented ${el.$type}`
+    }
+}
+
+class Binary implements Visitor, Expression {
+    operator ?: BinaryOp
+    expression : Expression[]
+    operands : Expression[]
+    constructor(el: BinaryT) {
+        this.expression = el.expression.map(ExpressionVisit)
+        this.operands = el.operands.map(ExpressionVisit)
+        this.operator = el.Operator
+    }
+}
+class GetClock implements Visitor, Expression {
+    constructor(el: GetClockT) {
+        throw `Not Implemented ${el.$type}`
+    }
+}
+class GetSensor implements Visitor, Expression {
+    constructor(el: GetSensorT) {
+        throw `Not Implemented ${el.$type}`
+    }
+}
+class GetSpeed implements Visitor, Expression {
+    constructor(el: GetSpeedT) {}
+}
+class Operation implements Visitor, Expression {
+    constructor(el: OperationT) {
+        throw `Not Implemented ${el.$type}`
+    }
+}
+class Unary implements Visitor, Expression {
+    constructor(el: UnaryT) {
+        throw `Not Implemented ${el.$type}`
+    }
+}
+class VariableRef implements Visitor, Expression {
+    ref : String
+    constructor(el: VariableRefT) {
+        this.ref = ReferenceParse(el.ref)
+    }
+}
+
+function ReferenceParse(el:Reference) {
+    return el.$refText
+}
+
+function ExpressionVisit(el: ExpressionT): Expression {
+    switch (el.$type) {
+        case "Binary": {
+            return new Binary(el as BinaryT)
+        }
+        case "FunctionCall": {
+            return new FunctionCall(el as FunctionCallT)
+        }
+        case "GetClock": {
+            return new GetClock(el as GetClockT)
+        }
+        case "GetSensor": {
+            return new GetSensor(el as GetSensorT)
+        }
+        case "GetSpeed": {
+            return new GetSpeed(el as GetSpeedT)
+        }
+        case "Operation": {
+            return new Operation(el as OperationT)
+        }
+        case "Unary": {
+            return new Unary(el as UnaryT)
+        }
+        case "VariableRef": {
+            return new VariableRef(el as VariableRefT)
+        }
+        default: {
+            throw `Not defined ${el.$type}`
+        }
+    }
+}
+
+function StatementVisit(el: StatementT): Statement {
+    switch (el.$type) {
+        case "Assignation": {
+            return new Assignation(el as AssignationT)
+        }
+        case "Backward": {
+            return new Backward(el as BackwardT)
+        }
+        case "Block": {
+            return new Block(el as BlockT)
+        }
+        case "Condition": {
+            return new Condition(el as ConditionT)
+        }
+        case "Forward": {
+            return new Forward(el as ForwardT)
+        }
+        case "FunctionCall": {
+            return new FunctionCall(el as FunctionCallT)
+        }
+        case "FunctionDeclaration": {
+            return new FunctionDeclaration(el as FunctionDeclarationT)
+        }
+        case "Leftward": {
+            return new Leftward(el as LeftwardT)
+        }
+        case "Mouvement": {
+            return new Mouvement(el as MouvementT)
+        }
+        case "Rightward": {
+            return new Rightward(el as RightwardT)
+        }
+        case "Rotate": {
+            return new Rotate(el as RotateT)
+        }
+        case "SetClock": {
+            return new SetClock(el as SetClockT)
+        }
+        case "VariableDeclaration": {
+            return new VariableDeclaration(el as VariableDeclarationT)
+        }
+        case "SetSpeed": {
+            return new SetSpeed(el as SetSpeedT)
+        }
+        default: {
+            throw `Not defined ${el.$type}`
+        }
+    }
+}
+
+const services = createRobotMlServices(EmptyFileSystem);
+const parse = parseHelper(services.RobotMl);
+
+const document = await parse(`
+Void main(entree : Float) {
+
+    let a : Integer = GetSpeed()
+    let b : Integer = GetSpeed()
+
+    let c = main()
+
+    if ( main() ) {
+        let e : Void = GetSpeed()
+    } 
+    elif ( (== (== a b) c) ) {
+        let e : Void = GetSpeed()
+    }
+    elif ( (((((((((b))))))))) ) {
+        let e : Void = GetSpeed()
+    }
+    elif ( == main() main() ) {
+        let e : Void = GetSpeed()
+    }
+    else {
+
+    }
+}
+`, { validation: true }); //enable validation, otherwise the validator will not be called!
+
+const ast = new Robot(document.parseResult.value as RobotMLT)
+console.log(JSON.stringify(ast,null,2))
